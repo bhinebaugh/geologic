e288bbdf041d9bb2a031774be506dc65b4a88557
Byron Hinebaugh <bhinebaugh@gmail.com>
2011-05-12 18:37:25 -0400

    Added 'show all' and 'hide all' links

--- timeline.js
+++ timeline.js
@@ -1,11 +1,20 @@
 $(document).ready( function() {
-	// in the list #eons, hide any child lists
-	$('.shown').hide();
-	$('#eons').find('ol').hide(); //makes dd's show on dt-mouseout
 	// set the descriptions for each list item (i.e. a dd element) to (hidden?)
-	// and toggle the class to display them in the sidebar on mouseover
-	//$('#eons').find('ol').hide();
-	$('#eons dt').hover( 
+	$('.shown').hide();
+	hideOLs = function() {
+		// in the list #eons, hide any child lists
+		$('#eons').find('ol').hide();
+		console.log('hiding OLs');
+	};
+	hideOLs();
+	$('#hideall').click( hideOLs );
+	$('#showall').click( function() {
+		$('#eons').find('ol').show();
+		console.log('showing OLs');
+	});
+		
+	// toggle visibility of description (<dd>) when the title (<dt>) is moused over 
+	$('#eons dt').hover(
 		function(){
 			$(this).next().show();
 			return false;
@@ -18,11 +27,9 @@ $(document).ready( function() {
 	
 	//expand eras when clicked, and show child list	
 	$('.era').click( function() { 
+		
 		if ( $(this).find('ol:first').css('display') == 'none' ) {
 			$(this).children('ol').show() ;
-			//hide OL by adding/removing a class instead
-			//$('#details h2').html("About the "+$(this).find('dt')[0].innerHTML);
-			//$('#description').html($(this).find('dd:first')[0].innerHTML)
 			dd1 = $(this).find('dd:first')[0]
 			$(dd1).toggleClass('child-shown');
 		} else {
