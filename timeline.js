ec948a106a3cd2d9a429c804df085d72d5337218
Byron Hinebaugh <bhinebaugh@gmail.com>
2011-08-03 23:06:16 -0400

    updated stuff, iirc

--- timeline.js
+++ timeline.js
@@ -1,41 +1,85 @@
+var scaling = {
+	'precambrian' : { 'width':'115%', 'left': 0 },
+	'phanerozoic' : { 'width':'600%', 'left':'-500%' },
+	'paleozoic' : { 'width' : '1350%', 'left': '-1167%'},
+	'mesozoic' : { 'width':'1650%', 'left':'-1540%' },
+	'cenozoic' : { 'width' : '4000%', 'left' : '-3900%' }
+}
+
+
 $(document).ready( function() {
 	// set the descriptions for each list item (i.e. a dd element) to (hidden?)
 	$('.shown').hide();
 	hideOLs = function() {
 		// in the list #eons, hide any child lists
 		$('#eons').find('ol').hide();
-		console.log('hiding OLs');
+		$('.era').removeClass('open-arrow');
+		$(this).addClass('greyed');
+		$('#showall').removeClass('greyed')
 	};
 	hideOLs();
 	$('#hideall').click( hideOLs );
 	$('#showall').click( function() {
 		$('#eons').find('ol').show();
-		console.log('showing OLs');
+		// change arrows to open
+		$('.era').addClass('open-arrow');
+		// grey out link text
+		$(this).addClass('greyed');
+		$('#hideall').removeClass('greyed');
 	});
-		
-	// toggle visibility of description (<dd>) when the title (<dt>) is moused over 
-	$('#eons dt').hover(
-		function(){
+	showAbout = function(){
 			$(this).next().show();
 			return false;
-		},
-		function() {
+	};
+	hideAbout = function() {
 			$(this).next().hide();
 			return false;
+	};
+	zoomTimeline = function(eraName) {
+		console.log("Zooming to "+eraName);
+		if ( scaling[eraName] ) {
+			$('#horizontal-ribbon').css( scaling[eraName] );
+		} else {
+			console.log("not a key in scaling object");
+			return false;
 		}
-	);
+	}
+	// toggle visibility of description (<dd>) when the title (<dt>) is moused over 
+	/**** this was not unbinding properly
+	/ $('dt').hover(
+		showAbout, //mouseover
+		hideAbout //mouseout
+	); */
+	$('dt').bind( 'mouseover', showAbout).bind( 'mouseout', hideAbout);
+	$("#make-plain").click( function() {
+		$('.shown').show();
+		$('dd').removeClass('shown');
+		$('ol').show();
+		// remove hover show/hide functionality
+		//$('dt').hover( function() {return false}, function() {return false});
+		//hideAbout = function() { console.log("new hideAbout function"); return false };
+		//showAbout = function() { return false }
+		$('dt').unbind( 'mouseover', showAbout ).unbind( 'mouseout', hideAbout );
+	});
 	
 	//expand eras when clicked, and show child list	
 	$('.era').click( function() { 
-		
 		if ( $(this).find('ol:first').css('display') == 'none' ) {
-			$(this).children('ol').show() ;
+			$(this).addClass('open-arrow');
+			$('#hideall').removeClass('greyed');
+			$(this).children('ol').show();
 			dd1 = $(this).find('dd:first')[0]
 			$(dd1).toggleClass('child-shown');
+			// zoom timeline ribbon to the era in question
+			zoomTimeline( $(this.firstChild).attr('class') );
 		} else {
+			$(this).removeClass('open-arrow');
+			$('#showall').removeClass('greyed');
 			$(this).children('ol').hide();
 		}
 		return false;
 	});
-	$('.period').click( function() { return false; /**prevent the click event from going to parent and hiding this element**/ });
+	$('.period').click( function() { 
+		return false; /**prevent the click event from going to parent and hiding this element**/ 
+	});
 });
